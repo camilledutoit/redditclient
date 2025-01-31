3000be41f2b7c9935f4f5d3b0ad3688734607df5
Camille Dutoit <185546222+camilledutoit@users.noreply.github.com>
2025-01-31 11:17:58 -0600

    root element media query

--- src/Components/Root/Root.jsx
+++ src/Components/Root/Root.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import { NavLink } from 'react-router-dom';
 import styles from './Root.module.css';
+import { Menu, X } from 'lucide-react'
+import { useState } from 'react';
 
 function Root() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
     <div>
       <header className={styles.header}>
         <nav className={styles.nav}>
           <p className={styles.logo}>Reddit Client</p>
-          <ul className={styles.navList}>
+          <ul className={styles.desktopNav}>
             <li>
               <NavLink
                 to="/"
@@ -35,9 +39,47 @@ function Root() {
               </NavLink>
             </li>
           </ul>
+          <button 
+            className={styles.menuButton}
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
+          >
+            {isMenuOpen ? <X size={24} /> : <Menu size={24} />}
+          </button>
+
+          {/* Mobile Menu */}
+          {isMenuOpen && (
+            <ul className={styles.mobileNav}>
+              <li>
+                <NavLink
+                  to="/"
+                  className={({ isActive }) =>
+                    isActive
+                      ? `${styles.navLink} ${styles.activeNavLink}`
+                      : styles.navLink
+                  }
+                  onClick={() => setIsMenuOpen(false)}
+                >
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/about"
+                  className={({ isActive }) =>
+                    isActive
+                      ? `${styles.navLink} ${styles.activeNavLink}`
+                      : styles.navLink
+                  }
+                  onClick={() => setIsMenuOpen(false)}
+                >
+                  About
+                </NavLink>
+              </li>
+            </ul>
+          )}
         </nav>
       </header>
-      <main className={styles.main}>
+      <main>
         <Outlet />
       </main>
     </div>
