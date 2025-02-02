5efa3cfddbf3cac328ed1199a2821baf7e1a435c
Camille Dutoit <185546222+camilledutoit@users.noreply.github.com>
2025-02-02 11:19:44 -0600

    changed input and description font color and added ABOUT section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Root from './Components/Root/Root.jsx'
 import Search from './Features/Search/Search.jsx'
 import Results from './Features/Results/Results.jsx'
 import Subreddit from './Components/Subreddit/Subreddit.jsx'
+import About from './Features/About/About.jsx'
 
 const router = createBrowserRouter(createRoutesFromElements(
 <Route path="/" element={<Root/>}>  
@@ -12,6 +13,7 @@ const router = createBrowserRouter(createRoutesFromElements(
     <Route index element={<Results/>}/>
     <Route path=":subreddit" element={<Subreddit/>}/>
   </Route>
+  <Route path="about" element={<About/>}/>
 </Route>
 ))
 
---
+++ src/Features/About/About.jsx
@@ -0,0 +1,9 @@
+function About() {
+    return (
+        <div>
+            <h1>ME LA PELAN</h1>
+        </div>
+    )
+}
+
+export default About
